5508b5239bd3285fc3e93346e9bf407a38947b5a
codeteenage <1830702951@qq.com>
2022-06-07 22:24:11 +0800

    网关的添加

--- cloud-provider-order/src/main/java/cn/com/codeteenager/cloudproviderorder/CloudProviderOrderApplication.java
+++ cloud-provider-order/src/main/java/cn/com/codeteenager/cloudproviderorder/CloudProviderOrderApplication.java
@@ -3,13 +3,17 @@ package cn.com.codeteenager.cloudproviderorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableFeignClients
 public class CloudProviderOrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudProviderOrderApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(CloudProviderOrderApplication.class, args);
+        String username = applicationContext.getEnvironment().getProperty("user.name");
+        String age = applicationContext.getEnvironment().getProperty("user.age");
+        System.out.println("user.name===="+username+",user.age===="+age);
     }
 
 }
